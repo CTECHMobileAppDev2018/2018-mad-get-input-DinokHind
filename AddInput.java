922e6e22940c52308f5e81ddbec92ed8d676c1a1
Dinok Hind <32515334+DinokHind@users.noreply.github.com>
2019-02-13 17:24:58 -0600

    Installation corrected, added argument limit

--- AddInput.java
+++ AddInput.java
@@ -7,20 +7,18 @@ public class AddInput {
         // The variable "args" holds all of the arguments that were passed into this program
         // The variable "args" is called an "array" because it's actually a list of items
         // If you good "How to get the number of items in an array in Java", you should be able to figure out how many integers there are in the variable "args"
-        // int num_args = ???
-        // if (num_args != 2) {
-        //    do something
-        // }
-        // More advanced: What if the two inputs aren't integers? Then what? How cold you check that?
-    
+        int num_args = args.length;
+        if (num_args != 2) {
+        System.out.println("Please only input two numbers");
+        } else {
         // **** START HERE: ****
         // These two lines will get you the first two arguments 
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int sum = a + b;
-        System.out.println("The sum of " + a + " and " + b + " is " + sum);
         
         // Print out "The sum of _ and _ is _" where you actually print the numbers they enter and the sum
-
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+        }
     }
 }
